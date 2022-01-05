978a26c0972184d7b57550ce75961c3bde7e46ff
Chunchun <yccvivo@gmail.com>
2022-01-05 14:23:00 -0600

    feat: add list on welcome page and tutorial page

--- ui/welcome_page.go
+++ ui/welcome_page.go
@@ -8,6 +8,16 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+const listHeight = 20
+
+type item struct {
+	title, desc string
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
 type WelcomePage struct {
 	Tabs       []string
 	TabContent []list.Model
@@ -21,7 +31,22 @@ func NewWelcomePage() WelcomePage {
 		"Tutorial",
 	}
 
-	return WelcomePage{Tabs: tabs}
+	items_welcome := []list.Item{
+		item{title: "Show Plugins", desc: "All the plugins supported by Telegraf"},
+		item{title: "Show Flags", desc: "Flags come with Telegraf"},
+	}
+
+	items_tutorial := []list.Item{
+		item{title: "How Telegraf works", desc: "..."},
+		item{title: "Set up", desc: "..."},
+		item{title: "Showing me around", desc: "..."},
+	}
+
+	var tabcontent []list.Model
+	defaultWidth = 40
+	tabcontent = append(tabcontent, list.NewModel(items_welcome, list.NewDefaultDelegate(), defaultWidth, listHeight))
+	tabcontent = append(tabcontent, list.NewModel(items_tutorial, list.NewDefaultDelegate(), defaultWidth, listHeight))
+	return WelcomePage{Tabs: tabs, TabContent: tabcontent}
 }
 
 func (w *WelcomePage) Update(m tea.Model, msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -43,7 +68,9 @@ func (w *WelcomePage) Update(m tea.Model, msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, nil
 		}
 	}
-	return m, nil
+	var cmd tea.Cmd
+	w.TabContent[w.activatedTab], cmd = w.TabContent[w.activatedTab].Update(msg)
+	return m, cmd
 }
 
 func (w *WelcomePage) View() string {
@@ -73,8 +100,16 @@ func (w *WelcomePage) View() string {
 		}
 	}
 
+	// Welcome message
+	{
+		_, err := doc.WriteString("Welcome to Telegraf!\n\n")
+		if err != nil {
+			return err.Error()
+		}
+	}
+
 	// list
-	_, err := doc.WriteString("Hello I am a welcome page")
+	_, err := doc.WriteString(w.TabContent[w.activatedTab].View())
 	if err != nil {
 		return err.Error()
 	}
