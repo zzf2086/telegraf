62734d1ee32057eb8b558aa32a4bc5436e6db787
zhongfeng.zhang <zhongfeng.zhang@funplus.com>
2022-02-10 12:14:05 +0800

    update format

--- plugins/inputs/aliyuncms/discovery.go
+++ plugins/inputs/aliyuncms/discovery.go
@@ -15,7 +15,7 @@ import (
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/responses"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/polardb"
-        r_kvstore "github.com/aliyun/alibaba-cloud-sdk-go/services/r-kvstore"
+	r_kvstore "github.com/aliyun/alibaba-cloud-sdk-go/services/r-kvstore"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/rds"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/slb"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/vpc"
@@ -124,9 +124,9 @@ func newDiscoveryTool(regions []string, project string, lg telegraf.Logger, cred
 		case "acs_polardb":
 			dscReq[region] = polardb.CreateDescribeDBClustersRequest()
 			responseObjectIDKey = "DBClusterId"
-                case "acs_kvstore":
-                        dscReq[region] = r_kvstore.CreateDescribeInstancesRequest()
-                        responseObjectIDKey = "InstanceId"
+		case "acs_kvstore":
+			dscReq[region] = r_kvstore.CreateDescribeInstancesRequest()
+			responseObjectIDKey = "InstanceId"
 		case "acs_memcache":
 			return nil, noDiscoverySupportErr
 		case "acs_ocs":
